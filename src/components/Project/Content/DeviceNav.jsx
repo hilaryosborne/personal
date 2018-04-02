1b97735027d8fdb1005d1effd064d0c99e29ac43
Hilary Osborne <h.t.osborne@gmail.com>
2018-04-02 15:14:20 +1000

    Finished device nav component

--- src/components/Project/Content/DeviceNav.jsx
+++ src/components/Project/Content/DeviceNav.jsx
@@ -2,24 +2,49 @@ import React from 'react'
 import radium from 'radium'
 import PropType from 'prop-types'
 import classnames from 'classnames'
+import _ from 'lodash'
+import {colors} from 'scripts/styles'
+
+const styling = {
+  el: {
+    fontSize: '2.4rem',
+    opacity: '.7',
+    marginLeft: '.8rem',
+    marginRight: '.8rem',
+    color: colors.tomato,
+    ':hover': {
+      opacity: '1'
+    }
+  },
+  active: {
+    opacity: '1'
+  }
+}
 
 @radium
-export class Tag extends React.Component {
+export class DeviceNav extends React.Component {
   static propTypes = {
-    // project: PropType.any,
+    icon: PropType.string,
+    active: PropType.bool,
     style: PropType.object,
-    className: PropType.string
+    className: PropType.string,
+    onClick: PropType.func
   }
 
   static defaultProps = {
-    // project: false,
+    icon: 'fas fa-laptop',
+    active: false,
     style: {},
-    className: ''
+    className: '',
+    onClick: _.noop
+  }
+
+  getActiveStyles () {
+    if (!this.props.active) { return {} }
+    return {...styling.active}
   }
 
   render () {
-    return (<div className={classnames(this.props.className)} style={[this.props.style]}>
-      Component
-    </div>)
+    return (<i className={classnames(this.props.icon, this.props.className)} style={{...this.props.style, ...styling.el, ...this.getActiveStyles()}} onClick={this.props.onClick} />)
   }
 }
