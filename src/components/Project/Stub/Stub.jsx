8ecda873fae93b1111afd2f2e92b6887d84abfff
Hilary Osborne <h.t.osborne@gmail.com>
2018-04-03 13:14:41 +1000

    Added project stub structure

--- src/components/Project/Stub/Stub.jsx
+++ src/components/Project/Stub/Stub.jsx
@@ -10,6 +10,8 @@ import { Summary } from './Summary'
 
 const styling = {
   wrapper: {
+    paddingTop: '2rem',
+    paddingBottom: '2rem',
     '@media print': {
       pageBreakInside: 'avoid'
     }
@@ -23,18 +25,26 @@ export class Stub extends React.Component {
     }
 
     render () {
-      return (<div style={{...styling.wrapper, marginBottom: '2.5rem'}}>
-        <Title project={this.props.project} />
-        <div className='d-block d-md-flex mt-2'>
-          <Tags project={this.props.project} />
-          <Time project={this.props.project} />
+      return (<div style={{...styling.wrapper}} className='d-flex align-items-center' >
+        <div style={{marginRight: '2.5rem', marginLeft: '1.5rem'}}>
+          <div style={{position: 'relative'}}>
+            <i className='fas fa-code' style={{fontSize: '1.3rem', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}} />
+            <i className='far fa-circle' style={{fontSize: '3rem'}} />
+          </div>
         </div>
-        <Summary project={this.props.project} className='mt-2' />
-        {_.get(this.props.project, 'links', []).length > 0 ? <div className='links mt-2'>
-          {_.map(_.get(this.props.project, 'links'), (link, k) => {
-            return <Link key={k} link={link} />
-          })}
-        </div> : null}
+        <div>
+          <Title project={this.props.project} />
+          <div className='d-block d-md-flex mt-2'>
+            <Tags project={this.props.project} />
+            <Time project={this.props.project} />
+          </div>
+          <Summary project={this.props.project} className='mt-2' />
+          {_.get(this.props.project, 'links', []).length > 0 ? <div className='links mt-2'>
+            {_.map(_.get(this.props.project, 'links'), (link, k) => {
+              return <Link key={k} link={link} />
+            })}
+          </div> : null}
+        </div>
       </div>)
     }
 }
